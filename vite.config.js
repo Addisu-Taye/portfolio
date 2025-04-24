f1b3892f8d895d880dc62038a2733a038a9d1ac1
Addisu-Taye <addtaye@gmail.com>
2025-04-24 17:01:25 +0300

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
-
-// Conditionally set the base path depending on the environment
-const isGitHubPages = process.env.GITHUB_PAGES === 'true';
-
+// Set the base URL for GitHub Pages deployment
+const isGitHubPages = process.env.GITHUB_PAGES === 'true'; // You can also check the environment manually
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(), // Tailwind v4+ official plugin
-  ],
-  base: isGitHubPages ? '/portfolio/' : '/', // Change 'portfolio' to your repo name
-  resolve: {
-    alias: {
-      '@': '/src',
+    plugins: [
+        react(),
+        tailwindcss(), // Tailwind v4+ official plugin
+    ],
+    base: isGitHubPages ? '/portfolio/' : '/', // Change 'portfolio' to your repository name
+    resolve: {
+        alias: {
+            '@': '/src',
+        },
     },
-  },
 });
